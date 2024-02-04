03d93115a7715e5b1c09303cafdd38db75902a5d
romb38 <r.barbier38@gmail.com>
2024-02-04 14:21:47 +0100

    Ajout de la liste d'élément

--- app/src/main/java/com/example/standouda/MainActivity.kt
+++ app/src/main/java/com/example/standouda/MainActivity.kt
@@ -8,19 +8,22 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -44,52 +47,82 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// Définition de votre modèle Item
+data class Item(val name: String)
+
 @Preview
 @Composable
 fun DiceRollerApp(){
+
+    // État de la liste des items
+
+    var itemList by remember { mutableStateOf(generateItemList()) }
+
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        // Afficher le rectangle violet en haut de l'écran avec du texte à l'intérieur
+        // Afficher la TopBar
+        TopBar()
         Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(100.dp)
-                .background(color = colorResource(id = R.color.purple_200).copy(alpha = 0.5f)),
-            contentAlignment = Alignment.CenterStart
+            modifier = Modifier.weight(1f)
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier.fillMaxWidth()
+            // Afficher la liste d'items avec un espace en haut pour la TopBar
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
             ) {
-                Text(
-                    modifier = Modifier.padding(16.dp),
-                    text = stringResource(R.string.app_name),
-                    style = TextStyle(fontSize = 24.sp, color = Color.White),
-                )
-                IconButton(onClick = { /* TODO */ }) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.more_vert), // Assurez-vous de mettre l'icône appropriée
-                        contentDescription = stringResource(R.string.more_options),
-                        tint = Color.White
-                    )
+                items(itemList.size) { index ->
+                    ListItem(item = itemList[index])
                 }
+            }
+            // Bouton de rafraîchissement en bas à droite
+            Box(
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+            ) {
+                RefreshButton()
             }
         }
 
-        Spacer(modifier = Modifier.weight(1f))
 
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentSize(Alignment.BottomEnd),
-            contentAlignment = Alignment.BottomEnd
+    }
+}
+
+@Composable
+fun TopBar(modifier: Modifier = Modifier){
+// Afficher le rectangle violet en haut de l'écran avec du texte à l'intérieur
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .background(color = colorResource(id = R.color.purple_200).copy(alpha = 0.5f)),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth()
         ) {
-            RefreshButton(modifier = Modifier.fillMaxSize())
+            Text(
+                modifier = Modifier.padding(16.dp),
+                text = stringResource(R.string.app_name),
+                style = TextStyle(fontSize = 24.sp, color = Color.White),
+            )
+            IconButton(
+                modifier = Modifier.padding(16.dp),
+                onClick = { /* TODO */ }
+            )
+            {
+                Icon(
+                    modifier = Modifier.size(40.dp),
+                    painter = painterResource(id = R.drawable.more_vert), // Assurez-vous de mettre l'icône appropriée
+                    contentDescription = stringResource(R.string.more_options),
+                    tint = Color.White
+                )
+            }
         }
     }
 }
+
 
 @Composable
 fun RefreshButton(modifier: Modifier = Modifier){
@@ -98,8 +131,8 @@ fun RefreshButton(modifier: Modifier = Modifier){
             .padding(40.dp)
             .size(80.dp),
         shape = CircleShape,
-        onClick = {/*TODO*/}
-    ){
+        onClick = {/*TODO*/ },
+    ) {
         Icon(
             painter = painterResource(id = R.drawable.outline_update),
             contentDescription = "Settings Icon",
@@ -107,4 +140,20 @@ fun RefreshButton(modifier: Modifier = Modifier){
         )
     }
 
+}
+
+// Générateur de liste d'items pour la démonstration
+fun generateItemList(): List<Item> {
+    return List(20) { index -> Item("Item_${index + 1}") }
+}
+
+@Composable
+fun ListItem(item: Item) {
+    Text(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        text = item.name,
+        style = TextStyle(fontSize = 24.sp, color = Color.White)
+    )
 }
